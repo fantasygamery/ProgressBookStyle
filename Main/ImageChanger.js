516bfda2b7c54b7668302130591416bcd72ac726
fantasygamery <30944486+fantasygamery@users.noreply.github.com>
2017-08-31 05:54:57 +0700

    Update ImageChanger.js

--- Main/ImageChanger.js
+++ Main/ImageChanger.js
@@ -1,10 +1,33 @@
 var Images = {
-"https://parentaccess.ocps.net/images/public/parentaccess.jpg":'https://raw.githubusercontent.com/fantasygamery/ProgressBookStyle/master/Main/PGLogo.jpg',
+"https://parentaccess.ocps.net/images/public/parentaccess.jpg":function(E){
+  E.id = 'time';
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+  t = setTimeout(function() {
+    startTime()
+  }, 500);
+}
+startTime();
+
+},
 }
 var all = document.getElementsByTagName("img");
 for (var i=0, max=all.length; i < max; i++) {
 if (Images[all[i].src]){
-all[i].src = Images[all[i].src];
+Images[all[i].src](all[i]);
 console.log('Revised: '+i)
 }
 }
